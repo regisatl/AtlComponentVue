8bcca16da71fe782362f04e126a874172128b7d3
regisatl <regisattolou21@outlook.fr>
2024-04-18 18:09:36 +0100

    en cours

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Alert from '@/components/frontendComponents/Alert.vue'
+import Step from '@/components/frontendComponents/Step.vue'
 </script>
 
 <template>
   <main>
     <Alert />
+    <Step />
   </main>
 </template>
